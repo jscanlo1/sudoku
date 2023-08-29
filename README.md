d15cfbd4297d2a5545526d15c0909444dc90f849
Johnny <johnnyscanlon22@gmail.com>
2023-08-30 00:50:26 +0100

    Installed raylib package

---
+++ README.md
@@ -0,0 +1,8 @@
+# Sudoku with c++
+
+This project uses the SMFL graphics library to generate a game of sudoku!
+
+Compiled using gcc through mingw-64
+raylib can be installed through pacman
+
+## Principles of sudoku
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 #include <string>
-#include <cmath>
-#include <array>
-
+#include <raylib.h>
 
 int main(int argc, char *argv[])
 {
     std::cout << "Hello world!" << std::endl;
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60); 
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
